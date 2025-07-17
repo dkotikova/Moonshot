1be5b109b65835a97410546b825fb66b57f13050
dkotikova <dskotikova@gmail.com>
2025-07-17 18:28:20 +0300

    Initial Commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Дарья on 17.07.2025.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
